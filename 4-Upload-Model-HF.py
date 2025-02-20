dd76ccd1cd9910658ea69837cc4f5bd792fe6f4d
Ganapathy Subramaniam Sundar <105225199+GanapathySubramaniam@users.noreply.github.com>
2025-02-20 14:01:46 -0500

    Update 4-Upload-Model-HF.py

--- 4-Upload-Model-HF.py
+++ 4-Upload-Model-HF.py
@@ -3,7 +3,7 @@ import torch
 
 def upload_model_to_hf(model_output_dir,username,new_model_name):
   torch.cuda.empty_cache()
-  login(token='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')
+  login(token='hfXXXX')
   create_repo(new_model_name)
   api = HfApi()
   api.upload_folder(
@@ -12,10 +12,3 @@ def upload_model_to_hf(model_output_dir,username,new_model_name):
       repo_type="model"
   )
 
-
-# if __name__=="__main__":
-#   upload_config={
-#       'username':'Ganapathy0112357',
-#       'new_model_name':'sample_model_123456',
-#       'model_output_dir':output_path}
-#   upload_model_to_hf(**upload_config)
